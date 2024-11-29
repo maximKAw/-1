abed9a2019f8ce6a1c6ffd3dc6a836db6f83d4a6
maximKAw <168087872+maximKAw@users.noreply.github.com>
2024-11-29 21:00:27 +0900

    Initial commit

---
+++ ЮщишинТест1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using InspectorLib;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionInsp inspector = new FunctionInsp();
+
+            // Проверка главного инспектора
+            Console.WriteLine(inspector.GetInspector());
+
+            // Проверка названия автоинспекции
+            Console.WriteLine(inspector.GetCarInspection());
+
+            // Изменение главного инспектора
+            inspector.SetInspector("Иванов И.И.");
+            Console.WriteLine(inspector.GetInspector());
+
+            // Генерация номера
+            Console.WriteLine(inspector.GenerateNumber(1234, 'A', 75));
+
+            // Получение списка сотрудников
+            var workers = inspector.GetWorker();
+            Console.WriteLine(string.Join(", ", workers));
+
+            // Добавление нового сотрудника
+            inspector.AddWorker("Петров П.П.");
+            Console.WriteLine(string.Join(", ", inspector.GetWorker()));
+        }
+    }
+}
